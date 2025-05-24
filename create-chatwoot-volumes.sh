d0146025332cf68857a06aff80da41fef353c966
tendenciadigitalio <jetmx35@gmail.com>
2025-05-24 17:15:25 -0600

    Delete create-chatwoot-volumes.sh

--- create-chatwoot-volumes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Crea todos los volúmenes necesarios para Chatwoot manualmente
-docker volume create chatwoot_storage
-docker volume create chatwoot_public
-docker volume create chatwoot_mailer
-docker volume create chatwoot_mailers
-docker volume create chatwoot_pgdata
-docker volume create chatwoot_redisdata
